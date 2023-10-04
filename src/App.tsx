98879dd97d676c27aa827b0a950bec65d2a5067b
Ayush Bansal <bansalayush8279@gmail.com>
2023-10-04 12:16:43 +0530

    ic

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-// import { useState } from 'react'
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './components/Home'
-import Form from './components/Form'
+// import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Registration_form from "./components/Registration_form";
+import Home from "./components/Home";
 
 function App() {
 
+  // const urlr = import.meta.env.VITE_REGION;
   return (
+    <>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='form/' element={<Form/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="form" element={<Registration_form/>}/>
       </Routes>
     </BrowserRouter>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Registration_form.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import './Registration_form.css'; 
+
+function Registration_form() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // You can handle form submission logic here
+    console.log('Form submitted with data:', formData);
+  };
+
+  return (
+    <div>
+        <img src='src/assets/tm_logo2.svg' alt='logo' style={{width:"150px"}}/>
+      <h2>Registration Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Registration_form;
